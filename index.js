5c0b7f29d39bc281d8920711eb9a4853f3efe68d
Jonathan Kulak <lackadaize@msn.com>
2024-01-10 11:00:43 -0500

    This is the commit for the Javascript Event Listeners Lab assignment.

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 function addingEventListener () {
-
-const input = document.getElementById('button');
-
-function clickAlert() {
-  alert('I was clicked!');
-}
-
-input.addEventListener('click', clickAlert);
+  const input = document.getElementById('button');
+  function clickAlert() {
+    return alert('I was clicked!');
+  }
+  input.addEventListener('click', clickAlert);
 }
